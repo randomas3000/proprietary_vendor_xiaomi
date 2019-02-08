54a4ff8b302e3034210c1b6acdcbaa306bb4ed05
Revanth <princechalapathi16@gmail.com>
2019-02-08 13:48:04 +0000

    Import MIUI Camera From My Repo - Lazy Af To Clone

---
+++ MiuiCamera/rosy.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, vendor/xiaomi/MiuiCamera/config.mk)
+
+VENDOR_PATH := vendor/xiaomi/MiuiCamera
+
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/system/etc/device_features/rosy.xml:system/etc/device_features/rosy.xml
+
