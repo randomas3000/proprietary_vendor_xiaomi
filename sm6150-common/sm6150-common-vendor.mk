34895eb82418a523f14f0016abea04537a4095ae
akeleyoyo77 <hernan.mauricio16@gmail.com>
2022-02-17 21:31:20 +0100

    sm6150-common: drop wfdcommon

--- sm6150-common/sm6150-common-vendor.mk
+++ sm6150-common/sm6150-common-vendor.mk
@@ -542,6 +542,5 @@ PRODUCT_PACKAGES += \
     dpmserviceapp \
     ims \
     qcrilmsgtunnel \
-    WfdCommon \
     manifest_android.hardware.drm@1.3-service.widevine \
     vendor.qti.gnss@4.0-service
